6542a378cef6684bf83d019e052cb2ec3edb9a65
roaa12114 <roaa12114@gmail.com>
2024-11-22 00:43:07 +0100

    Initial commit

---
+++ task2.js
@@ -0,0 +1,9 @@
+// Optional: Smooth Scroll for Anchor Links (if needed)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
